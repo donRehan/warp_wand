291c1dded9794e70bd8efe0bcc42f55bbd67bee4
DonRehan <30264386+donRehan@users.noreply.github.com>
2024-05-27 12:59:03 +0300

    Add error message on translation not available

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule , provideHttpClient } from '@angular/common/http';
 import { TranslatorComponent } from './translator/translator.component';
+import {
+  MissingTranslationHandler, MissingTranslationHandlerParams ,
+  TranslateService , TranslateModule, TranslateLoader
+}
+from '@ngx-translate/core';
+
+export class MyMissingTranslationHandler implements MissingTranslationHandler {
+    handle(params: MissingTranslationHandlerParams) {
+        return 'Translation not found for ' + params.key + 'contact support';
+    }
+}
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -22,7 +32,9 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppRoutingModule,
     HttpClientModule,
     TranslateModule.forRoot({
-        defaultLanguage: 'en',
+        missingTranslationHandler: {provide: MissingTranslationHandler,
+        useClass: MyMissingTranslationHandler},
+        //defaultLanguage: 'en',
         loader: {
             provide: TranslateLoader,
             useFactory: HttpLoaderFactory,
